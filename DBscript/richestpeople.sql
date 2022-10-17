225a0d9a20831351dfde579725891a25bae39376
PercyFalco <330187@student.mboutrecht.nl>
2022-10-17 10:29:57 +0200

    final commit

---
+++ DBscript/richestpeople.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Gegenereerd op: 17 okt 2022 om 08:27
+-- Serverversie: 5.7.31
+-- PHP-versie: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mvc-oop-toets`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `richestpeople`
+--
+
+DROP TABLE IF EXISTS `richestpeople`;
+CREATE TABLE IF NOT EXISTS `richestpeople` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(200) NOT NULL,
+  `Networth` varchar(200) NOT NULL,
+  `MyAge` tinyint(4) NOT NULL,
+  `Company` varchar(200) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
